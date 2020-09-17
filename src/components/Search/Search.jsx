fb09135f3b83917dc3fbd6db45a08f71c4a74bfb
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-09-17 14:36:55 +0530

    added react context api

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,13 +4,22 @@ import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import { useStateValue } from "../../StateProvider";
+import { actionTypes } from "../../reducer";
 
 export default function Search({ hideButtons = false }) {
+  // eslint-disable-next-line
+  const [{}, dispatch] = useStateValue();
+
   const [input, setInput] = useState("");
   const history = useHistory();
 
   const search = (e) => {
     e.preventDefault();
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
     history.push("/search");
   };
 
@@ -21,6 +30,7 @@ export default function Search({ hideButtons = false }) {
         <input value={input} onChange={(e) => setInput(e.target.value)} />
         <MicIcon className="search-micIcon" />
       </div>
+
       {!hideButtons ? (
         <div className="search-buttons">
           <Button type="submit" onClick={search} variant="outlined">
