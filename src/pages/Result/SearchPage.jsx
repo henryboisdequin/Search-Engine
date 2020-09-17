cef5aaf3cf020a440fd9845bee706a8f51d7cb13
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-09-17 16:58:34 +0530

    api calls to frontend

--- src/pages/Result/SearchPage.jsx
+++ src/pages/Result/SearchPage.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./SearchPage.css";
 import { useStateValue } from "../../StateProvider";
+import useGoogleSearch from "../../useGoogleSearch";
+import { Link } from "react-router-dom";
+import Search from "../../components/Search/Search";
 
 export default function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
+  // const { data } = useGoogleSearch(term);
+  const data = Response;
 
+  console.log(data);
   return (
     <div className="searchPage">
       <div className="searchPage-header">
-        <h1>You searched for {term}</h1>
+        <Link to="/">
+          <img
+            className="searchPage-logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          ></img>
+        </Link>
+        <div className="searchPage-headerBody">
+          <Search hideButtons />
+        </div>
       </div>
       <div className="searchPage-results"></div>
     </div>
