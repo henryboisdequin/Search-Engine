77baeb0f39ef58ca8a450a1e1ba68c4a98865f1e
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-09-17 09:00:25 +0530

    finished search bar

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 export default function Search() {
   return (
@@ -9,7 +10,11 @@ export default function Search() {
       <div className="search-input">
         <SearchIcon className="search-inputIcon" />
         <input />
-        <MicIcon />
+        <MicIcon className="search-micIcon" />
+      </div>
+      <div className="search-buttons">
+        <Button variant="outlined">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
